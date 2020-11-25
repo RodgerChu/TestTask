96f6e4d79f9157375798c1d1d5ce36917b7bd3b3
Rodger <gailichv@yandex.ru>
2020-11-25 22:46:21 +0300

    Added extenstion to System.Random, created RandomAppender, object for testing

---
+++ Assets/Scripts/RandomAppender.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class RandomAppender
+{
+    public void AppendToArray(GameObject[] source, GameObject[] additional)
+    {
+        var nullIndexes = GetNullElementsIndexes(source);
+
+        if (nullIndexes.Length == 0)
+            return;
+
+        var random = new System.Random();
+        random.Shuffle(nullIndexes);
+        random.Shuffle(additional);
+
+        int i = 0, j = 0;
+
+        while (i < nullIndexes.Length && j < additional.Length)
+        {
+            var nullIndex = nullIndexes[i];
+            var newElement = additional[j];
+
+            source[nullIndex] = newElement;
+
+            i++;
+            j++;
+        }
+    }
+
+    private int[] GetNullElementsIndexes(GameObject[] objects)
+    {
+        var indexes = new List<int>();
+        
+        for (int i = 0; i < objects.Length; i++)
+        {
+            var obj = objects[i];
+
+            if (obj == null)
+            {
+                indexes.Add(i);
+            }
+        }
+
+        return indexes.ToArray();
+    }
+}
---
+++ Assets/Scripts/Tester.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tester : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] _sourceObjects;
+
+    [SerializeField]
+    private GameObject[] _additionalObjects;
+
+    private void Start()
+    {
+        var randomAppender = new RandomAppender();
+        randomAppender.AppendToArray(_sourceObjects, _additionalObjects);
+    }
+}
